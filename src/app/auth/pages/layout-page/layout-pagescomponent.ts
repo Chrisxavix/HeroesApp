7e4bdca8aeec9cbe647445199eb34f4cdb22a7cc
Chrisxavix <chrischillogallid64@outlook.com>
2024-05-27 22:34:08 -0500

    Build routing in differents modules

--- src/app/auth/pages/layout-page/layout-pagescomponent.ts
+++ src/app/auth/pages/layout-page/layout-pagescomponent.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-layout-pages',
-  templateUrl: './layout-pages.component.html',
-  styleUrl: './layout-pages.component.css'
+  templateUrl: './layout-page.component.html',
+  styleUrl: './layout-page.component.css'
 })
 export class LayoutPagesComponent {
 

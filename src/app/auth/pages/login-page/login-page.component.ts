cf9e7bb6eb9579b776aaf9358c68f8dca7ba3201
Chrisxavix <chrischillogallid64@outlook.com>
2024-06-03 21:45:11 -0500

    guards new

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-login-page',
@@ -7,4 +10,18 @@ import { Component } from '@angular/core';
 })
 export class LoginPageComponent {
 
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+  ) {
+
+  }
+
+  onLogin(): void {
+    this.authService.login("1").subscribe(resp => {
+      if(!resp) return;
+      console.log(resp, " login");
+      this.router.navigate(['/heroes/list'])
+    })
+  }
 }
--- src/app/heroes/pages/layout-page/layout-page.component.ts
+++ src/app/heroes/pages/layout-page/layout-page.component.ts
@@ -1,14 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../../auth/services/auth.service';
+import { User } from '../../../auth/interfaces/user.interfaces';
 
 @Component({
   selector: 'app-layout-page',
   templateUrl: './layout-page.component.html',
   styleUrl: './layout-page.component.css'
 })
-export class LayoutPageComponent {
+export class LayoutPageComponent implements OnInit{
+
+  public user?: User;
+
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+  ) {
+
+  }
+
+  ngOnInit(): void {
+    this.user = this.authService.currentUser;
+  }
+
   public sidebarItems = [
     { label: "Listado", icon: "label", url: "./list" },
     { label: "Añadir", icon: "add", url: "./new-hero" },
     { label: "Buscar", icon: "search", url: "./search" }
   ]
+
+  logout(): void {
+    this.authService.logout();
+    this.router.navigate(["auth"]);
+  }
+
+
 }
